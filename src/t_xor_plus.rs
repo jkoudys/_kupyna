0d8d9407056fb0518a9400b200f0d7046191d8c2
AnarchistHoneybun <thatonebipanda@gmail.com>
2024-06-21 08:26:45 +0530

    t_plus_l func replicating paper now

--- src/t_xor_plus.rs
+++ src/t_xor_plus.rs
@@ -38,6 +38,21 @@ fn add_constant_xor(mut state: Matrix, round: usize) -> Matrix {
     state
 }
 
+fn add_constant_plus(mut state: Matrix, round: usize) -> Matrix {
+    let state_ptr = state.as_mut_ptr() as *mut u64;
+
+    for j in 0..COLS {
+        unsafe {
+            *state_ptr.add(j) = state_ptr.add(j).read().wrapping_add(
+                0x00F0F0F0F0F0F0F3u64 ^
+                    (((((COLS - j - 1) * 0x10) ^ round) as u64) << 56)
+            );
+        }
+    }
+
+    state
+}
+
 fn s_box_layer(mut state: Matrix) -> Matrix {
     for i in 0..ROWS {
         for j in 0..COLS {
@@ -115,6 +130,32 @@ pub fn t_xor_l(block: &[u8], rounds: usize) -> Vec<u8> {
     for nu in 0..rounds {
         println!("round[{}].input: {:02x?}", nu, state);
         state = add_constant_xor(state, nu);
+        println!("round[{}].add_c: {:02x?}", nu, state);
+        state = s_box_layer(state);
+        println!("round[{}].s_box: {:02x?}", nu, state);
+        state = rotate_rows(state);
+        println!("round[{}].s_byt: {:02x?}", nu, state);
+        state = mix_columns(state);
+        println!("round[{}].m_col: {:02x?}", nu, state);
+    }
+    matrix_to_block(state)
+}
+
+/// Placeholder for the T+l transformation.
+///
+/// # Arguments
+///
+/// * `block` - A byte slice representing the block to be transformed.
+/// * `_rounds` - The number of rounds to perform.
+///
+/// # Returns
+///
+/// * A `Vec<u8>` containing the transformed block.
+pub fn t_plus_l(block: &[u8], rounds: usize) -> Vec<u8> {
+    let mut state = block_to_matrix(block);
+    for nu in 0..rounds {
+        println!("round[{}].input: {:02x?}", nu, state);
+        state = add_constant_plus(state, nu);
         println!("round[{}].add_c: {:02x?}", nu, state);
         state = s_box_layer(state);
         println!("round[{}].s_box: {:02x?}", nu, state);
